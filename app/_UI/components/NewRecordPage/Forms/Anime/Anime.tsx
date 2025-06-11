f40cdb6e97f35ff7f98cd3782d39d0649b065d58
juan-soares <juan.css013@gmail.com>
2025-06-11 08:52:12 -0300

    anime form

--- app/_UI/components/NewRecordPage/Forms/Anime/Anime.tsx
+++ app/_UI/components/NewRecordPage/Forms/Anime/Anime.tsx
@@ -2,6 +2,42 @@ export function AnimeForm() {
   return (
     <form>
       <h1>Adicionar Anime</h1>
+
+      <fieldset>
+        <legend>Ficha Técnica</legend>
+        <label htmlFor="title">Título:</label>
+        <input type="text" required id="title" name="title" />
+
+        <label htmlFor="title">Título Traduzido:</label>
+        <input
+          type="text"
+          required
+          id="translatedTitle"
+          name="translatedTitle"
+        />
+
+        <label htmlFor="release">Lançamento:</label>
+        <input type="date" required id="release" name="release" />
+
+        <label htmlFor="synopsis">Sinopse:</label>
+        <textarea required id="synopsis" name="synopsis" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Arquivos</legend>
+        <label htmlFor="cover">Capa:</label>
+        <input type="file" required id="cover" name="cover" />
+
+        <label htmlFor="trailer">Trailer:</label>
+        <input type="file" required id="trailer" name="trailer" />
+
+        <label htmlFor="images">Imagens:</label>
+        <input type="file" multiple id="images" name="images" />
+
+        <label htmlFor="attachments">Anexos:</label>
+        <input type="file" multiple id="attachments" name="attachments" />
+      </fieldset>
+      <button>Enviar</button>
     </form>
   );
 }
