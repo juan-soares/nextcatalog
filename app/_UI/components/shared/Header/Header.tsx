2647cd1b5f19c908c84ecd43fdd70777e989a078
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2025-10-25 02:58:01 +0000

    feat: loginbar

--- app/_UI/components/shared/Header/Header.tsx
+++ app/_UI/components/shared/Header/Header.tsx
@@ -1,12 +1,13 @@
 import styles from "./Header.module.css";
 import { Logo } from "../Logo";
-import { Searchbar } from "./Searchbar";
+import { Searchbar, Loginbar } from "./";
 
 export async function Header() {
   return (
     <header className={styles.header}>
       <Logo />
       <Searchbar />
+      <Loginbar />
     </header>
   );
 }
---
+++ app/_UI/components/shared/Header/Loginbar/Loginbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import styles from "./Loginbar.module.css";
+
+export function Loginbar() {
+  const user = null;
+
+  if (!user) {
+    return (
+      <div className={styles.loginbar}>
+        <Link href="/login">
+          <button>Entrar</button>
+        </Link>
+      </div>
+    );
+  }
+
+  const { avatar, nickname } = user;
+
+  return (
+    <div className={styles.loginbar}>
+      <img src={avatar} />
+      <span>{nickname}</span>
+      <button>Sair</button>
+    </div>
+  );
+}
