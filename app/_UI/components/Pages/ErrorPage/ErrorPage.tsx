fabc9ecde78599136c4e8b52e10d44997ad160b1
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2025-10-26 13:05:20 +0000

    feat:error

---
+++ app/_UI/components/Pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,21 @@
+"user client";
+
+import Link from "next/link";
+import styles from "./ErrorPage.module.css";
+
+export function ErrorPage() {
+  return (
+    <div className={styles.errorPage}>
+      <img
+        width={50}
+        height={50}
+        src="./img/icons/error-icon.png"
+        alt="Ícone de mapa indicando que a página não foi encontrada."
+      />
+      <p>
+        <strong>Ops!</strong> A página que está procurando não existe, ou não
+        está disponível. Que tal <Link href="/">voltar</Link>?
+      </p>
+    </div>
+  );
+}
---
+++ app/error.tsx
@@ -0,0 +1,7 @@
+
+
+import { ErrorPage } from "./_UI/components/Pages";
+
+export default function Error() {
+  return <ErrorPage />;
+}
