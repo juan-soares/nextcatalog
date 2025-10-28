a2c0a4dc141f057c4372a4e6a771948e1e7507fb
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2025-10-28 01:18:21 +0000

    feat:login page

--- app/_UI/components/Pages/index.ts
+++ app/_UI/components/Pages/index.ts
@@ -1,2 +1,3 @@
 export * from "./HomePage";
 export * from "./ErrorPage";
+export * from "./LoginPage";
---
+++ app/login/page.tsx
@@ -0,0 +1,5 @@
+import { LoginPage } from "../_UI/components/Pages";
+
+export default function Login() {
+  return <LoginPage />;
+}
