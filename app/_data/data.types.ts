cb8955a367cd76be9c7ca8bc44af429813c1ea79
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2025-11-17 02:03:00 +0000

    test

--- app/_data/data.types.ts
+++ app/_data/data.types.ts
@@ -1,6 +1,23 @@
+export interface IFindParameters {
+  query?: { fieldsToSearch: string[]; termsToSearch: string[] };
+  limit?: number;
+  sortBy?: "alph" | "recent" | "release";
+}
+
 export interface IDatabase {
   categories: ICategory[];
   animes: IAnime[];
+}
+
+export interface IDoc {
+  _id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IRecord extends IDoc {
+  translatedTitle: string;
 }
 
 export interface ICategory extends IRecord {
@@ -11,17 +28,4 @@ export interface ICategory extends IRecord {
 export interface IAnime extends IRecord {
   slug: string;
   collection: keyof IDatabase;
-}
-
-export interface IRecord {
-  _id: string;
-  title: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-export interface IFindParameters {
-  query?: { fieldsToSearch: string[]; termsToSearch: string[] };
-  limit?: number;
-  sortBy?: "alph" | "recent" | "release";
 }
