b1218bef9c1d4b7c7ecc17ba4caf46a496c4b785
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2025-11-21 01:47:45 +0000

    a

--- src/_features/Header/TopHeader/Userbar/Userbar.actions.ts
+++ src/_features/Header/TopHeader/Userbar/Userbar.actions.ts
@@ -1,10 +1,6 @@
 "use server";
 
 import { deleteCurrentUser } from "@/src/_lib/session/user";
-import { redirect } from "next/navigation";
-import { revalidatePath } from "next/cache";
-
 export async function logout() {
   deleteCurrentUser();
- 
 }
