b1aea478bf0c61da403c5db235d8afedf2eb3f22
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2025-11-23 05:03:50 +0000

    test

--- src/_features/Header/TopHeader/TopHeader.tsx
+++ src/_features/Header/TopHeader/TopHeader.tsx
@@ -1,11 +1,13 @@
 import { Logo } from "@/src/_UI/components";
 import { Searchbar } from ".";
+import { Userbar } from "../../Auth/Userbar";
 
 export function TopHeader() {
   return (
     <div>
       <Logo />
       <Searchbar />
+      <Userbar/>
     </div>
   );
 }
