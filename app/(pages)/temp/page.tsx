72cc8754fa715d275613ae6c75e975d4cbe41be8
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2025-12-18 22:00:35 +0000

    aa

--- app/(pages)/temp/page.tsx
+++ app/(pages)/temp/page.tsx
@@ -1,24 +1,38 @@
 import { categories } from "./temp.const";
 
-export default function TempPage() {
+export default function TempPage({
+  searchParams,
+}: {
+  searchParams: { category?: string };
+}) {
+  const category = searchParams.category || null;
+
+  if (!category) {
+    return (
+      <div>
+        <form>
+          {categories.length === 0 && <p>Sem categorias para exibir.</p>}
+          {categories.length > 0 &&
+            categories.map(({ _id, title, collection }) => (
+              <div key={_id}>
+                <input
+                  type="checkbox"
+                  id={_id}
+                  name="category"
+                  value={collection}
+                />
+                <label htmlFor={_id}>{title}</label>
+              </div>
+            ))}
+          <button>Enviar</button>
+        </form>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <form>
-        {categories.length === 0 && <p>Sem categorias para exibir.</p>}
-        {categories.length > 0 &&
-          categories.map(({ _id, title, collection }) => (
-            <div key={_id}>
-              <input
-                type="checkbox"
-                id={_id}
-                name="category"
-                value={collection}
-              />
-              <label htmlFor={_id}>{title}</label>
-            </div>
-          ))}
-        <button>Enviar</button>
-      </form>
+      <h1>{category}</h1>
     </div>
   );
 }
