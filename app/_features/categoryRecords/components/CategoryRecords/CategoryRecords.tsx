f507e4cb075de3844585081d993d0f04ccf164ad
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2025-12-29 01:08:44 +0000

    asasa

--- app/_features/categoryRecords/components/CategoryRecords/CategoryRecords.tsx
+++ app/_features/categoryRecords/components/CategoryRecords/CategoryRecords.tsx
@@ -1,3 +1,4 @@
+import styles from "./CategoryRecords.module.css";
 import { IDatabase } from "@/app/_data/data.types";
 import { getCategoryRecordsByCollection } from "./CategoryRecords.actions";
 import Link from "next/link";
@@ -16,34 +17,78 @@ export async function CategoryRecords({
   const categoryRecords = await getCategoryRecordsByCollection(collection);
 
   return (
-    <div>
-      <h1>{categoryTitle}</h1>
-      <aside></aside>
-      <main>
-        {categoryRecords.length === 0 && (
-          <section>
-            <p>
-              Sem itens na lista.
-              <Link href={`${categorySlug}/novo`}>Adicionar?</Link>
-            </p>
-          </section>
-        )}
-        {categoryRecords.length > 0 && (
-          <ul>
-            {categoryRecords.map(
-              ({ _id, slug, cover, title, releaseYear, synopsis }) => (
-                <li key={_id} title={synopsis}>
-                  <Link href={slug}>
-                    <img src={cover} alt={`Capa do título ${title}.`} />
-                    <strong>{title}</strong>
-                    <span>{releaseYear}</span>
-                  </Link>
-                </li>
-              )
-            )}
-          </ul>
-        )}
-      </main>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{categoryTitle}</h1>
+
+      <div className={styles.layout}>
+        {/* Sidebar */}
+        <aside className={styles.sidebar}>
+          <form>
+            <input type="search" placeholder="Pesquisar..." />
+            <Link href={`${categorySlug}/novo`}>
+              <button type="button">Novo</button>
+            </Link>
+          </form>
+          <form className={styles.filters}>
+            <fieldset>
+              <legend>Filtro 1</legend>
+              <label>
+                <input type="checkbox" /> Opção 1
+              </label>
+            </fieldset>
+
+            <fieldset>
+              <legend>Filtro 2</legend>
+              <label>
+                <input type="checkbox" /> Opção 1
+              </label>
+            </fieldset>
+
+            <fieldset>
+              <legend>Filtro 3</legend>
+              <label>
+                <input type="checkbox" /> Opção 1
+              </label>
+            </fieldset>
+          </form>
+        </aside>
+
+        {/* Conteúdo */}
+        <main className={styles.main}>
+          {categoryRecords.length === 0 && (
+            <section className={styles.empty}>
+              <p>
+                Sem itens na lista.{" "}
+                <Link href={`${categorySlug}/novo`}>Adicionar?</Link>
+              </p>
+            </section>
+          )}
+
+          {categoryRecords.length > 0 && (
+            <section>
+              <div className={styles.sortBar}>
+                <button>A-Z</button>
+                <button>Lançamento</button>
+                <button>Recente</button>
+              </div>
+
+              <ul className={styles.grid}>
+                {categoryRecords.map(
+                  ({ _id, slug, cover, title, releaseYear, synopsis }) => (
+                    <li key={_id} className={styles.card} title={synopsis}>
+                      <Link href={slug}>
+                        <img src={cover} alt={`Capa do título ${title}.`} />
+                        <strong>{title}</strong>
+                        <span>{releaseYear}</span>
+                      </Link>
+                    </li>
+                  )
+                )}
+              </ul>
+            </section>
+          )}
+        </main>
+      </div>
     </div>
   );
 }
