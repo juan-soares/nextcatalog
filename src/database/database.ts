c802d09908c778c306276b975242743ec2daa055
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2026-03-09 11:09:18 +0000

    db cache

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,11 +4,18 @@ import { Database, MongoDoc } from "@/database/types";
 
 const DB_PATH = path.join(process.cwd(), "src/database/db.json");
 
+let cachedDb: Database | null = null;
+
 async function connect(): Promise<Database> {
+  if (cachedDb) return cachedDb;
+
   if (!DB_PATH) throw new Error("Caminho incorreto do banco de dados.");
 
   const fileContent = await fs.readFile(DB_PATH, "utf-8");
-  return JSON.parse(JSON.stringify(JSON.parse(fileContent))) as Database;
+  const db: Database = JSON.parse(fileContent);
+  cachedDb = db;
+
+  return cachedDb;
 }
 
 async function write(db: Database): Promise<void> {
@@ -21,11 +28,13 @@ async function write(db: Database): Promise<void> {
         createdAt: (item as any).createdAt || now,
         updatedAt: now,
       }));
+
       return [collectionName, updatedItems];
     }),
-  );
+  ) as Database;
 
   await fs.writeFile(DB_PATH, JSON.stringify(dbToWrite, null, 2));
+  cachedDb = dbToWrite;
 }
 
 export const database = { connect, write };
