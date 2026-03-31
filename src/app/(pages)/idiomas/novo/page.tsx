aa68d80bfccaa493224365e9d36d3751ade53332
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2026-03-31 22:44:57 +0000

    asa

--- src/app/(pages)/idiomas/novo/page.tsx
+++ src/app/(pages)/idiomas/novo/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  await fetch("/api/idiomas");
+};
+
 export default function Page() {
   return (
     <main>
       <h1>Novo Idioma</h1>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="title">Valor</label>
         <input id="title" name="title" type="text" required />
         <button>Enviar</button>
---
+++ src/app/api/idioma/route.ts
@@ -0,0 +1,5 @@
+export async function POST(req: Request) {
+  console.log(req);
+
+  return Response.json({});
+}
